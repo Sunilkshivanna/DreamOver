02b90fafa09b08cc9e9f8f58806145931434dd25
Your Name <your-email@example.com>
2025-03-07 15:36:16 +0530

    Updated login page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Home() {
-    return <h1>Welcome to Cricket Gaming App</h1>;
+    return <h1>Welcome to Cricket Gaming App - Deployed</h1>
+    ;
 }
 
 export default Home;
